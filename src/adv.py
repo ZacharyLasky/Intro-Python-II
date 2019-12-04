f9da9191629739a1228c01562a557ae2c67a5dca
Zach Lasky <zacharylasky123@gmail.com>
2019-12-04 18:19:54 -0500

    player starting point

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player1 = Player("Player", room["outside"].name)
+beginRoom = player1.current_room
+
 
 # Write a loop that:
 #
@@ -49,3 +53,15 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def game():
+    print(f"Welcome to adventure game! You start at the {beginRoom}")
+    while True:
+
+        inp1 = input("enter n, e, s, w to move OR q to quit")
+
+
+if __name__ == '__main__':
+    game()
+
+
